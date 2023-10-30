c7eca9620ce5f476348b45e284cc2ef71965d6e3
Gabriel Viernes <viernesgabriel@gmail.com>
2023-10-30 17:23:39 -0500

    reworked checks for invalid logo properties so that logo generation doesn't complete if an error is found

---
+++ README.md
@@ -0,0 +1,57 @@
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+# Logo Maker
+
+## Description
+
+The purpose of this project is to generates logos based on user specifications to demonstrate knowledge of OOP
+
+# Table of Contents
+
+- [Installation](#Installation)
+- [Usage](#Usage)
+- [Contributing](#Contributing)
+- [Tests](#Tests)
+- [License](#License)
+- [Questions](#Questions)
+
+# Installation
+
+Simple clone the repository onto your computer
+
+# Usage
+
+Navigate to the cloned folder and run index.js using node
+
+# Contributing
+
+This project is not accepting contributions
+
+# Tests
+
+This project should be tested using jest
+
+# License
+
+MIT License
+
+Copyright © 2023
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+# Questions
+
+[Github](https://github.com/Gabriel Viernes)
+[Repo](https://github.com/Gabriel-Viernes/logo-maker)
+[Video](https://watch.screencastify.com/v/UmgxFrOSq6rBZ8jA76rE)
+
+Email: email@email.com
+
+Please contact me via either of these methods if you have additional questions about this product
+            
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const inquirer = require('inquirer');
 const shape = require('./lib/shapes.js')
 const fs = require('fs')
 function getInfo() {
+    // Asks user for their desired logo properties
     let answers = inquirer.prompt([
         {
             type: 'input',
@@ -29,54 +30,53 @@ function getInfo() {
             ]
         }
     ]).then((answers) => {
-        if(answers.logoChar.length > 3 || answers.logoChar.length < 3) {
-            console.log('Please input exactly three letters')
-            getInfo()
-        }
         let isColor = /^[0-9A-F]{6}$/i
         // ^ - ensures match starts from start of string
         // [0-9A-F] - checks each char whether they match the provided sets
         // {6} - checks whether match is 6 characters long
         // $ - ensures match ends at the end of string
         // i - allows case insensitivity
-        if(isColor.test(answers.textColor) === false) {
-            console.log('Invalid text color, please enter a valid hexadecimal color without "#"')
+        // Checks for invalid logo properties (text length and hexadecimal validity)
+        if(answers.logoChar.length > 3 || answers.logoChar.length < 3) {
+            console.log('Please input exactly three letters', '\n')
             getInfo()
-        }
-        if(isColor.test(answers.backColor) === false) {
-            console.log('Invalid background color, please enter a valid hexadecimal color without "#"')
+        } else if(isColor.test(answers.textColor) === false || isColor.test(answers.backColor) === false) {
+            console.log('Invalid text or background color, please enter a valid hexadecimal color without "#"', '\n')
             getInfo()
+        } else {
+            // A class is selected based on what shape the user has chosen, then an appropriate shape is written to logo.svg
+            switch (answers.shape) {
+                case 'square':
+                    let tempSquare = new shape.Square(answers.backColor, answers.textColor, answers.logoChar.toUpperCase())
+                    fs.writeFileSync('./examples/logo.svg', tempSquare.render(), (err) => {
+                        if(err) {
+                            console.debug(err)
+                        }
+                    })
+                    console.log('Generated logo.svg')
+                    break;
+                case 'triangle':
+                    let tempTri = new shape.Triangle(answers.backColor, answers.textColor,answers.logoChar.toUpperCase())
+                    fs.writeFile('./examples/logo.svg', tempTri.render(), (err) => {
+                        if(err) {
+                            console.debug(err)
+                        }
+                    })
+                    console.log('Generated logo.svg')
+                    break;
+                case 'circle':
+                    let tempCir = new shape.Circle(answers.backColor, answers.textColor, answers.logoChar.toUpperCase())
+                    fs.writeFile('./examples/logo.svg', tempCir.render(), (err) => {
+                        if(err) {
+                            console.debug(err)
+                        }
+                    })
+                    console.log('Generated logo.svg')
+                    break;
+            }
         }
-        
-        switch (answers.shape) {
-            case 'square':
-                let tempSquare = new shape.Square(answers.backColor, answers.textColor, answers.logoChar.toUpperCase())
-                fs.writeFileSync('./examples/logo.svg', tempSquare.render(), (err) => {
-                    if(err) {
-                        console.debug(err)
-                    }
-                })
-                console.log('Generated logo.svg')
-                break;
-            case 'triangle':
-                let tempTri = new shape.Triangle(answers.backColor, answers.textColor,answers.logoChar.toUpperCase())
-                fs.writeFile('./examples/logo.svg', tempTri.render(), (err) => {
-                    if(err) {
-                        console.debug(err)
-                    }
-                })
-                console.log('Generated logo.svg')
-                break;
-            case 'circle':
-                let tempCir = new shape.Circle(answers.backColor, answers.textColor, answers.logoChar.toUpperCase())
-                fs.writeFile('./examples/logo.svg', tempCir.render(), (err) => {
-                    if(err) {
-                        console.debug(err)
-                    }
-                })
-                console.log('Generated logo.svg')
-                break;
-        }
+
+
     })
 }
 getInfo()
